3e4d6d1d4b320dcb21909ad6e1e52bba6cf3666c
Eminem <58310848+thebadcoder96@users.noreply.github.com>
2024-06-18 20:54:17 -0500

    extract urls code

---
+++ src/module/extract_url.py
@@ -0,0 +1,40 @@
+from langchain_community.document_loaders import AsyncChromiumLoader
+from langchain_community.document_transformers import BeautifulSoupTransformer
+import pandas as pd
+
+
+df = pd.read_csv("src/module/data/resources.csv")
+df.replace('\n', '', regex=True, inplace=True)
+df.replace(' ', '', regex=True, inplace=True)
+
+def get_categories():
+    return df['Category'].unique().tolist()
+
+def get_urls(category: str = 'Water', number: int = 1):
+    cat_df = df.loc[df['Category'] == category]
+    cat_df = cat_df.sample(number)['Resources']
+    return cat_df.values.tolist()
+
+def extract_urls(urls: list):
+    loader = AsyncChromiumLoader(urls)
+    docs = loader.load()
+    bs_transformer = BeautifulSoupTransformer()
+
+    docs_transformed = bs_transformer.transform_documents(docs,       
+            unwanted_tags=["script", "style", "footer", "header", "nav", "form", "noscript"],
+            remove_lines=True,
+            remove_comments=True)
+    
+    formatted = [
+        f"Source ID: {i}\nArticle Title: {doc.metadata}\nArticle Snippet: {doc.page_content}"
+        for i, doc in enumerate(docs_transformed)
+    ]
+    return formatted
+
+if __name__ == "__main__":
+    urls = get_urls(number=2)
+    formatted = extract_urls(urls)
+    print(formatted)
+
+
+
